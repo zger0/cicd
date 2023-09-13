f15558cab27cf8686f1282d836aaa0da17cfdf2b
zger <ehdwo223@gmail.com>
2023-09-13 22:00:12 +0900

    1

--- src/main/java/com/example/cicd/CicdApplication.java
+++ src/main/java/com/example/cicd/CicdApplication.java
@@ -2,6 +2,9 @@ package com.example.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class CicdApplication {
@@ -11,3 +14,4 @@ public class CicdApplication {
     }
 
 }
+
--- src/main/java/com/example/cicd/Controller.java
+++ src/main/java/com/example/cicd/Controller.java
@@ -1,2 +1,14 @@
-package com.example.cicd;public class Controller {
+package com.example.cicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @GetMapping("/ok")
+    public String ok(){
+        return "ok1";
+    }
 }
